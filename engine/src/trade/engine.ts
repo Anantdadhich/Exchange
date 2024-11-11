4f22fc661603dda1f8cf8693e4efa47ac501cd41
Anantdadhich <anantdadhich66@gmail.com>
2024-11-11 23:44:14 +0530

    engine

--- engine/src/trade/engine.ts
+++ engine/src/trade/engine.ts
@@ -1,5 +1,6 @@
 import fs  from "fs"
-import { OrderBook } from "./order"
+import { Order, OrderBook } from "./order"
+import { MessageFromApi } from "../types/fromApi"
 
 
 
@@ -46,4 +47,63 @@ export class Engine {
 
 
     }
+
+    saveSnapshot(){
+     const savsnapshotbook={
+        orderbooks:this.orderbooks.map(o => o.getSnapshot()),
+        balances:Array.from(this.balances.entries())
+
+     }
+     fs.writeFileSync("/.snapshot.json",JSON.stringify(savsnapshotbook))
+    }
+
+    process({message,clientId}:{message:MessageFromApi,clientId:string}){
+      switch(message.type){
+        case "CREATE_ORDER":
+            try {
+            const {orderId,executedQty,fills}=this.createOrder();
+
+            } catch (error) {
+                
+            }
+        case "CANCEL_ORDER":
+        case "ON_RAMP":
+        case "GET_DEPTH":
+        case "GET_OPEN_ORDERS":
+      }
+        
+
+    }
+    
+    addOrderbookm(orderbook:OrderBook){
+       this.orderbooks.push(orderbook)
+    }
+
+
+    createOrder(market:string,price:string,quantity:string,side:"buy"|"sell",userId:string){
+        const orderbook=this.orderbooks.find(o =>o.ticker()===market);
+        const baseasset=market.split("_")[0];
+        const qouteasset=market.split("_")[1];
+        
+        if(!orderbook ){
+            throw new Error("no order book ")
+        }
+        
+        
+        const order:Order={
+            price:Number(price),
+            quantity:Number(quantity),
+            orderId:Math.random().toString(15).substring(2,15) +Math.random().toString(15).substring(2,15) ,
+            filled:0,
+            side,
+            userId
+        }
+       
+        const {fills,executedQty}=orderbook.addOrder(order);
+
+
+        
+    }
+
+    
 }
